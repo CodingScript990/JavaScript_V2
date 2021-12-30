9ef25a1a7f6d3e721a793bea4d38ec5392cfda99
Minwookim <trte135@naver.com>
2021-12-30 23:16:51 +0900

    JavaScript_V2 - [12.30.2021] ✔ [File]  - app.js, app.css, app.scss
    ✔ [Study]
     - [operators(+,-,*,/,%,=), Data Types, let, const]

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,26 @@
-alert('This works!');
+const defaultResult = 0; // const inital value === 0
+let currentResult = defaultResult; // not change value
+
+currentResult += (10) * 3 / 2 - 1; // 10 * 3 = 30 / 2 = 15 - 1 = 14
+// defaultResult += (10) * 3 / 2 - 1; // typeError 0
+/*
+    [operators]
+    + : Add two numbers
+    - : Subtract two numbers
+    * : Multiply two numbers
+    / : Divide two numbers
+    % : Divide two numbers, yield remainder
+    ** : Exponentiation(e.g. 2 ** 3 = 8)
+    = : Assign value to variable
+*/
+
+let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1'; // String
+
+outputResult(currentResult, calculationDescription); 
+
+/*
+    [Data Types]
+    1. Numbers : 2,-2, 22.222 - Important for calculations and code
+    2. Strings(Text) : 'Hi', "Hi", `Hi` - Important for outputting results, gathering input
+    3. 
+*/
