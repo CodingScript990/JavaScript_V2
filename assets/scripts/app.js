c6120e32d033732622c44b9606423dbeb697837d
Minwookim <trte135@naver.com>
2022-01-01 22:15:35 +0900

    JavaScript_V2 - [1.1.2022] ✔ [File]  - app.js
    ✔ [Study]
     - [Function, Custom Function, Return Values, Importance of Code Order]

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,21 @@
 const defaultResult = 0; // const inital value === 0
-let currentResult = defaultResult; // not change value
+let currentResult; // not change value
 
-currentResult += (10) * 3 / 2 - 1; // 10 * 3 = 30 / 2 = 15 - 1 = 14
+// custom function
+
+function add(num1, num2) { // function name(parameter) { result; }
+    const rs = num1 + num2;
+    //alert('The result is ' + rs); // call(rs[const])
+    return rs; // element + element
+}
+
+currentResult = add(1, 2); // call(function)
+
+
+//currentResult += (10) * 3 / 2 - 1; // 10 * 3 = 30 / 2 = 15 - 1 = 14
 // defaultResult += (10) * 3 / 2 - 1; // typeError 0
 /*
-    [operators]
+[operators]
     + : Add two numbers
     - : Subtract two numbers
     * : Multiply two numbers
@@ -12,12 +23,14 @@ currentResult += (10) * 3 / 2 - 1; // 10 * 3 = 30 / 2 = 15 - 1 = 14
     % : Divide two numbers, yield remainder
     ** : Exponentiation(e.g. 2 ** 3 = 8)
     = : Assign value to variable
-*/
+    */
 
-let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`; // String
+    let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`; // String
 
-outputResult(currentResult, calculationDescription); 
+    outputResult(currentResult, calculationDescription); 
 
+
+   // let currentResult = defaultResult; [Error]
 /*
     [Data Types]
     1. Numbers : 2,-2, 22.222 - Important for calculations and code
