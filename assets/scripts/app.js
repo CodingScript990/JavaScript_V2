60ad1be5fc489e7c48985b84e78cb20b19d557fc
Minwookim <trte135@naver.com>
2022-01-02 22:55:51 +0900

    JavaScript_V2 - [1.2.2022] ✔ [File]  - app.js
    ✔ [Study]
     - [Global & Local Scope, return Statement, Executing Functions[Indirectly]]

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,16 +1,23 @@
 const defaultResult = 0; // const inital value === 0
-let currentResult; // not change value
+let currentResult = defaultResult; // not change value
+//let rs; // result value(initial)
 
 // custom function
 
-function add(num1, num2) { // function name(parameter) { result; }
-    const rs = num1 + num2;
+function add() { // function name(parameter) { result; }
+    currentResult = currentResult + userInput.value; // 0 + something??(input write...)
     //alert('The result is ' + rs); // call(rs[const])
-    return rs; // element + element
+    outputResult(currentResult, ''); // The output (0 + '' >> string >> Final output >> 01)
+    //return rs; // element + element
 }
 
-currentResult = add(1, 2); // call(function)
+// alert(rs); // reference error! To output, can call after calling a function!
 
+//currentResult = add(1, 2); // call(function)
+
+//alert(rs); // reference error! not available
+
+//currentResult = rs; // bad code!
 
 //currentResult += (10) * 3 / 2 - 1; // 10 * 3 = 30 / 2 = 15 - 1 = 14
 // defaultResult += (10) * 3 / 2 - 1; // typeError 0
@@ -25,9 +32,9 @@ currentResult = add(1, 2); // call(function)
     = : Assign value to variable
     */
 
-    let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`; // String
+    //let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`; // String
 
-    outputResult(currentResult, calculationDescription); 
+    addBtn.addEventListener('click', add); // addEventListener : function that causes an event
 
 
    // let currentResult = defaultResult; [Error]
